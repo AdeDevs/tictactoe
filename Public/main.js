24b75d90f5bc73f1958527d5cf1cf32c31354cb1
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-02-04 00:43:31 +0100

    Updated

---
+++ Public/main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYNDUNVdPK8sVriC_20w2GYfGXR3GU2_0",
+  authDomain: "kryptyktac.firebaseapp.com",
+  projectId: "kryptyktac",
+  storageBucket: "kryptyktac.appspot.com",
+  messagingSenderId: "420483400801",
+  appId: "1:420483400801:web:6baca12930561677f99620"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
